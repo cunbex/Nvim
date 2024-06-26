163f9f0b635a89df0c0b0a8ef5225dce2f9e40a0
cunbex <hamzalagab.tech@gmail.com>
2024-06-26 17:09:45 +0100

    feat(lazy, statusline): Add Lazy load up rules, Change statusbar layout

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,18 +1,33 @@
--- This file  needs to have same structure as nvconfig.lua 
+-- This file  needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
 local M = {}
 
 M.ui = {
-    theme = "chadracula-evondev",
-    transparency = true,
-    statusline = {
-        theme = "vscode_colored",
-        separator_style = "round",
-        order = nil,
-        modules = nil,
+  theme = "chadracula-evondev",
+  transparency = true,
+  statusline = {
+    theme = "default",
+    separator_style = "round",
+    order = {
+      "mode",
+      "file",
+      "git",
+      "%=",
+      "lsp_msg",
+      "%=",
+      "diagnostics",
+      "lsp",
+      "cursor",
+      "cwd",
     },
+    modules = {
+      cursor = function()
+        return "%#StText# Ln %l, Col %c  "
+      end,
+    },
+  },
 }
 
 return M
--- lua/configs/lazy.lua
+++ lua/configs/lazy.lua
@@ -1,7 +1,18 @@
 return {
   defaults = { lazy = true },
   install = { colorscheme = { "nvchad" } },
-
+  checker = {
+    enabled = true,
+    concurrency = 5, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+    check_pinned = false, -- check for pinned packages that can't be updated
+  },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = true, -- get a notification when changes are found
+  },
   ui = {
     icons = {
       ft = "",
@@ -12,6 +23,7 @@ return {
   },
 
   performance = {
+    reset_packpath = true,
     rtp = {
       disabled_plugins = {
         "2html_plugin",
